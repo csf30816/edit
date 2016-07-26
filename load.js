1e362b12f75a1a2728ebff3f1f338c5a6ac8bb2d
csf30816 <elijahporter@zoho.com>
2016-07-26 18:00:27 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,2 +1,2 @@
 var url = document.URL.substring(18, document.URL.length)
-document.getElementById("frame).innerHTML = 
+document.getElementById("frame").innerHTML = '<frame rows="100%" src="https://scratchx.org/?url=' + url + '#scratch"></frame>'
