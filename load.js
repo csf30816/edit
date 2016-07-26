bcb54bb52578ed7a39136193e244fff7f50d1096
csf30816 <elijahporter@zoho.com>
2016-07-26 18:40:35 -0500

    Delete load.js

--- load.js
+++
@@ -1,2 +0,0 @@
-var url = document.URL.substring(18, document.URL.length)
-document.getElementById("frame").innerHTML = '<frame rows="100%" src="https://scratchx.org/?url=' + url + '#scratch"></frame>'
